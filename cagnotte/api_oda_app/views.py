85514aa7e8ca6a78d3838979fc41f008049fee4f
Dydja <kadidjabayoko@gmail.com>
2022-02-10 13:58:50 +0000

    modif views

--- cagnotte/api_oda_app/views.py
+++ cagnotte/api_oda_app/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from snippets.serializers import SnippetSerializer
 from api_oda_app.models import Academician,Reason,Payment
-from rest_framework.decorators import api_view
+from rest_framework.decorators: import api_view
 
 # Create your views here.
 #First method GET of Academician
