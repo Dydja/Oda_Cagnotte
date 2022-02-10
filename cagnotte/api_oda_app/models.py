ea94fe03f9f9c7b138f9979b6e9452c0cdbf6df7
Dydja <kadidjabayoko@gmail.com>
2022-02-10 11:49:24 +0000

    model academicien

--- cagnotte/api_oda_app/models.py
+++ cagnotte/api_oda_app/models.py
@@ -1,3 +1,19 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+#model academicien
+
+class Academician(models.Model):
+    last_name = models.CharField(max_length=30 , verbose_name="nom")
+    first_name = models.CharField(max_length=40, verbose_name="prenom")
+    register_number = models.CharField(max_length=30,verbose_name="matricule")
+    picture = models.FileField(upload_to="pictures" , verbose_name="photos")
+
+    class Meta:
+        verbose_name = "Academicien"
+
+    
+    def __str__(self) -> str:
+        return self.last_name
