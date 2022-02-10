e62c0edd12113925e8c48ecf292f4292259630a2
Dydja <kadidjabayoko@gmail.com>
2022-02-10 13:32:00 +0000

    modification et creation du fichier serializer

--- cagnotte/api_oda_app/models.py
+++ cagnotte/api_oda_app/models.py
@@ -10,10 +10,10 @@ class Academician(models.Model):
     first_name = models.CharField(max_length=40, verbose_name="prenom")
     register_number = models.CharField(max_length=30,verbose_name="matricule")
     picture = models.FileField(upload_to="pictures" , verbose_name="photos")
-
+#qu'il renomme le nom de la table par academicine
     class Meta:
         verbose_name = "Academicien"
 
-    
+    #recupere l'object sous forme d'objects
     def __str__(self) -> str:
         return self.last_name
