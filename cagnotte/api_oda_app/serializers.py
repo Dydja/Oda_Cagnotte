e2a5debc8c368c567ea5a40651c189adb8b0eca5
Dydja <kadidjabayoko@gmail.com>
2022-02-10 13:57:26 +0000

    modfi serializer suite

--- cagnotte/api_oda_app/serializers.py
+++ cagnotte/api_oda_app/serializers.py
@@ -1,4 +1,5 @@
-from cagnotte.api_oda_app.models import Academician
+from pyexpat import model
+from cagnotte.api_oda_app.models import Academician, Reason,Payment
 from snippets.serializers import SnippetSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -8,4 +9,18 @@ from rest_framework.parsers import JSONParser
 class AcademicianSerializer(serializers.ModelSerializer):
     class Meta:
         model:Academician
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        exlude = ['date_add ','date_update','status']
+
+
+#serializer from Reasons
+
+class ReasonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model:Reason
+        exlude = ['date_add ','date_update','status']
+
+#serializer from Payment
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model:Payment
+        exlude = ['date_add ','date_update','status']
--- cagnotte/api_oda_app/views.py
+++ cagnotte/api_oda_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.serializers import SnippetSerializer
+from api_oda_app.models import Academician,Reason,Payment
+from rest_framework.decorators import api_view
 
 # Create your views here.
+#First method GET of Academician
+
