eae35615ba6722d2614d73556a11668c5c6e4a0b
amiroudiallo03 <amiroudiallo03@gmail.com>
2022-02-10 12:47:24 +0000

    commit

--- cagnotte/api_oda_app/models.py
+++ cagnotte/api_oda_app/models.py
@@ -1,3 +1,4 @@
+from email import charset
 from tabnanny import verbose
 from django.db import models
 
@@ -5,7 +6,16 @@ from django.db import models
 
 #model academicien
 
-class Academician(models.Model):
+class Base(models.Model):
+    date_add = models.DateField(auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Academician(Base):
     last_name = models.CharField(max_length=30 , verbose_name="nom")
     first_name = models.CharField(max_length=40, verbose_name="prenom")
     register_number = models.CharField(max_length=30,verbose_name="matricule")
@@ -13,7 +23,25 @@ class Academician(models.Model):
 
     class Meta:
         verbose_name = "Academicien"
-
+        verbose_name_plural = "Academiciens"
     
-    def __str__(self) -> str:
+    def __str__(self):
         return self.last_name
+
+class Motif(Base):
+    nom_motif = models.ForeignKey(Academician, related_name="motif_academician",verbose_name=("Academicien"), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Motif"
+        verbose_name_plural = "Motifs"
+
+    def __str__(self):
+        return self.nom_motif
+
+
+class payment(Base):
+    register_number = models.CharField(max_length=50)
+    motif = models.CharField(max_length=255)
+    montant = models.CharField(max_length=255)
+
+
